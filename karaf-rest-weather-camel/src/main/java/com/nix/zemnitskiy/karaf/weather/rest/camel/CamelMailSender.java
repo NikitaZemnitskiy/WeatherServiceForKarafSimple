9d0b52df6c15a85007a14a4d247a2621bc80fdb3
nikita <nik.zemnitskiy@gmail.com>
2020-10-07 13:50:00 +0300

    new version of karaf-rest-weather for karaf

--- karaf-rest-weather-camel/src/main/java/com/nix/zemnitskiy/karaf/weather/rest/camel/CamelMailSender.java
+++
@@ -1,7 +0,0 @@
-package com.nix.zemnitskiy.karaf.weather.rest.camel;
-
-import com.nix.zemnitskiy.karaf.weather.rest.api.Weather;
-
-public interface CamelMailSender {
-    public void sendEmail(Weather weather);
-}
--- karaf-rest-weather-camel/src/test/java/TestURI.java
+++
@@ -1,9 +0,0 @@
-import java.net.URI;
-import java.net.URISyntaxException;
-
-public class TestURI {
-    public static void main() throws URISyntaxException {
-        URI url = new URI("smtp://fake_mail:25");
-        System.out.println(url.getHost());
-    }
-}
