fa2a6c9564766dd6379269fc732df45d842f4728
nikita <nik.zemnitskiy@gmail.com>
2020-11-05 14:34:11 +0200

    read.me updated

--- karaf-rest-weather-api/src/main/java/com/nix/zemnitskiy/karaf/weather/rest/api/RandomNumber.java
+++ karaf-rest-weather-api/src/main/java/com/nix/zemnitskiy/karaf/weather/rest/api/RandomNumber.java
@@ -2,15 +2,8 @@ package com.nix.zemnitskiy.karaf.weather.rest.api;
 
 import org.apache.commons.math3.distribution.NormalDistribution;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
 
 public interface RandomNumber {
-
-    @Path("/randomNumber/{mean}/{standardDeviation}")
-    @Produces("application/json")
-    @GET
     NormalDistribution getRandomNumber (@PathParam("mean") int mean, @PathParam("standardDeviation") int standardDeviation);
 }
